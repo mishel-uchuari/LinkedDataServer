69104a7565d14481d7a9f7fea369b84007161835
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2016-12-02 16:51:22 +0100

    It works
    Redirect headers?

--- src/es/eurohelp/opendata/LinkedDataServer.java
+++ src/es/eurohelp/opendata/LinkedDataServer.java
@@ -24,6 +24,7 @@ public class LinkedDataServer extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append(request.getHeader("Accept"));
 	}
 
 	/**

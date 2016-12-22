147a07b4caff6678559d2972f11eb1170158c4d3
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2016-12-22 16:53:03 +0100

    Added web.xml and index.html

--- src/es/eurohelp/opendata/LinkedDataServer.java
+++ src/es/eurohelp/opendata/LinkedDataServer.java
@@ -1,6 +1,7 @@
 package es.eurohelp.opendata;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -19,41 +20,76 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 /**
- * LinkedDataServer Prototype, partly inspired by https://github.com/clarkparsia/sparql-proxy/blob/master/src/com/clarkparsia/sparql/SparqlEndpointProxy.java
+ * LinkedDataServer Prototype, partly inspired by
+ * https://github.com/clarkparsia/sparql-proxy/blob/master/src/com/clarkparsia/
+ * sparql/SparqlEndpointProxy.java
  * 
  * @author Mikel Egaña Aranguren
  * 
  */
-@WebServlet({ "/resource/*", "/class/*", "/property/*", "/value/*" })
+
+//@WebServlet({ "/*", "/resource/*", "/class/*", "/property/*", "/value/*" })
 public class LinkedDataServer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public LinkedDataServer() {}
+	public LinkedDataServer() {
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append(request.getHeader("Accept"));
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		// TODO: map external and internal URIs
 		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().println(request.getHeader("Accept"));
+//		response.getWriter().println(request.getRequestURI());
+//		response.getWriter().println(request.getRequestURL());
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+
+		// Write the response message, in an HTML page
+		try {
+			out.println("<!DOCTYPE html>");
+			out.println("<html><head>");
+			out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+			out.println("<title>Linked Data Server</title></head>");
+			out.println("<body>");
+			out.println("<h1>Linked Data Server</h1>"); // says Hello
+			// Echo client's request information
+			out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+			out.println("<p>Request URL: " + request.getRequestURL() + "</p>");
+			out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+			out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+			out.println("<p>Context Path: " + request.getContextPath() + "</p>");
+			out.println("<p>Accept header: " + request.getHeader("Accept") + "</p>");
+			out.println("</body>");
+			out.println("</html>");
+		} finally {
+			out.close(); // Always close the output writer
+		}
+		
+		
+		
+
 		String query = "DESCRIBE <http://opendata.euskadi.eus/AV-GASTEIZ/2016-02-06/PM10-AirQuality>";
 		String endpoint = "http://ckan.eurohelp.es:7200/repositories/opendata-euskadi?query=";
-		String complete_query = endpoint  + URLEncoder.encode(query);
-		
-		
+		String complete_query = endpoint + URLEncoder.encode(query);
+
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet(complete_query);
-		
+
 		Enumeration<String> req_headers = request.getHeaderNames();
 		while (req_headers.hasMoreElements()) {
 			String header_name = req_headers.nextElement();
 			String header_value = request.getHeader(header_name);
 			httpGet.setHeader(header_name, header_value);
 		}
-		
 
 		// TODO: redirect server response to response
 		CloseableHttpResponse triple_store_response = httpclient.execute(httpGet);
@@ -63,8 +99,12 @@ public class LinkedDataServer extends HttpServlet {
 		while (i.hasNext()) {
 			System.out.println(i.next());
 		}
-		
-		// TODO: render in proper HTML if no accept header or accept header something like application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*
+
+		// TODO: render in proper HTML if no accept header or accept header
+		// something like application/x-ms-application, image/jpeg,
+		// application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap,
+		// application/vnd.ms-excel, application/vnd.ms-powerpoint,
+		// application/msword, */*
 		System.out.println(entity_string);
 		triple_store_response.close();
 	}

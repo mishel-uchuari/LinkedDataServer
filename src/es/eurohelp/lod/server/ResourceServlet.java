39994a481167918a2465be0baffb6909e465b314
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-01-23 12:40:57 +0100

    First working prototype

--- src/es/eurohelp/lod/server/ResourceServlet.java
+++ src/es/eurohelp/lod/server/ResourceServlet.java
@@ -1,9 +1,11 @@
 package es.eurohelp.lod.server;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.text.MessageFormat;
 import java.util.HashMap;
@@ -12,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -21,6 +24,10 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -55,30 +62,42 @@ public class ResourceServlet extends HttpServlet {
 			if (accept.contains(MIMEtype.HTML.mimetypevalue())) {
 				accept = "application/rdf+xml";
 				
-				HttpResponse response = HttpManager.getInstance().doGetRequest(req, null, url, accept);
-				
-				Source             text        = new StreamSource(response.getEntity().getContent());
-				
-				ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			    InputStream stream = classLoader.getResourceAsStream("urumea.xsl");
-				
-			    
-			    LOGGER.info(stream.toString());
-			    
-			    Source             xslt        = new StreamSource(stream);
-			    TransformerFactory factory     = TransformerFactory.newInstance();
-			    Transformer        transformer = factory.newTransformer(xslt); 
-			    transformer.transform(text, new StreamResult(new File("output.html")));
-				
 //				POST http://localhost:9999/blazegraph/namespace/replicate/sparql?query=DESCRIBE+%3Chttp%3A%2F%2Fdonostia.eus%2Fdata%2Fid%2Fmedio-ambiente%2Fmedicion%2Furumea-txominenea-riesgo-2017-11-10-02-10%3E
 //			    http://localhost:8080/ROOT/id/medio-ambiente/medicion/urumea-txominenea-riesgo-2017-11-10-02-10
+				
+//				HttpResponse response = HttpManager.getInstance().doGetRequest(req, null, url, accept);
+				
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpGet httpget = new HttpGet(url);
+				httpget.setHeader("Accept", "application/rdf+xml");
+				HttpResponse blzgResponse = httpclient.execute(httpget);
+				
+
+				Source             text        = new StreamSource(blzgResponse.getEntity().getContent());				
+				ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+				InputStream stream = classLoader.getResourceAsStream("urumea.xsl");
+			    Source             xslt        = new StreamSource(stream);
 			    
-//				https://gist.github.com/serge1/9307868
-//			    Source             xslt        = new StreamSource(xsl);
-//			    Source             text        = new StreamSource(xml);
-//			    TransformerFactory factory     = TransformerFactory.newInstance();
-//			    Transformer        transformer = factory.newTransformer(xslt); 
-//			    transformer.transform(text, new StreamResult(new File("output.html")));
+			    TransformerFactory factory     = TransformerFactory.newInstance();
+			    Transformer        transformer = factory.newTransformer(xslt); 
+			    
+			    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			    StreamResult result = new StreamResult(baos);
+			    transformer.transform(text, result);
+			    
+			    
+			    
+			    resp.setContentType("text/html;charset=UTF-8");
+
+				PrintWriter out = resp.getWriter();
+
+
+				try {
+					out.print(result.getOutputStream().toString());
+				} finally {
+					out.close(); // Always close the output writer
+				}
+			    
 				
 				// TODO: integrar XSL transformer
 				// TODO: hacer el XSL generico (ver PHI base)
